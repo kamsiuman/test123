6bd70aa8d879102b7e6cb30190e6f90b1626e878
Alex Wong <kamsiuman@gmail.com>
2019-11-04 18:47:14 -0600

    this is 1st vresion

---
+++ p1.java
@@ -0,0 +1,8 @@
+public class p1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
+    }
+}
