d826e8a4070dd781561f37be1ab17eb13ab1697c
Alex Wong <kamsiuman@gmail.com>
2019-11-04 19:06:21 -0600

    add new class printer and use it in the main funtion

---
+++ Printer.java
@@ -0,0 +1,17 @@
+
+public class Printer {
+
+    public void PrintOnce() {
+        System.out.println("1111111");
+    }
+
+    public void PrintTwo() {
+        System.out.println("22222");
+    }
+
+    public void PrintN(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("NNNNNNNN");
+        }
+    }
+}
--- p1.java
+++ p1.java
@@ -1,5 +1,12 @@
 public class p1 {
+
     public static void main(String[] args) {
+
+        Printer myPrint = new Printer();
+
+        myPrint.PrintOnce();
+        myPrint.PrintTwo();
+        myPrint.PrintN(3);
         System.out.println("test");
         System.out.println("test2");
         System.out.println("test3");
